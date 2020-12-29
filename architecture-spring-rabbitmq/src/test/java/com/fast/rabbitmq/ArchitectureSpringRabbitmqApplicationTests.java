828401713ce4582725fa9002e96f152de66e56db
boyi.chen <495057454@qq.com>
2020-12-29 18:30:47 +0800

    rabbitmq 初始提交

---
+++ architecture-spring-rabbitmq/src/test/java/com/fast/rabbitmq/ArchitectureSpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureSpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
