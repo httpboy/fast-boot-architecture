b61a50b3b1c04aaecd1863fc730360856ce0541c
boyi.chen <495057454@qq.com>
2021-01-03 23:49:32 +0800

    elasticsearch案例

---
+++ architecture-spring-elasticsearch/src/main/java/com/fast/elasticsearch/ArchitectureSpringElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.fast.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureSpringElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureSpringElasticsearchApplication.class, args);
+    }
+
+}
