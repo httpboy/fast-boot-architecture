6562cab3faf3ca08e5cfe12eb7f8d771837e9525
boyi.chen <495057454@qq.com>
2021-01-09 18:07:29 +0800

    redisson封装分布式锁

---
+++ architucture-spring-lock/src/main/java/com/fast/lock/ArchituctureSpringLockApplication.java
@@ -0,0 +1,13 @@
+package com.fast.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchituctureSpringLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchituctureSpringLockApplication.class, args);
+    }
+
+}
---
+++ architucture-spring-lock/src/test/java/com/fast/lock/ArchituctureSpringLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchituctureSpringLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
