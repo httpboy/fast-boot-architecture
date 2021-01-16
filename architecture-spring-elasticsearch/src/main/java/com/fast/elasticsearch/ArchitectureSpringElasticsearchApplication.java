d9ea48a6aa29da4ce76a546decb5380d809418a0
boyi.chen <495057454@qq.com>
2021-01-16 21:28:01 +0800

    es版本注释

--- architecture-spring-elasticsearch/src/main/java/com/fast/elasticsearch/ArchitectureSpringElasticsearchApplication.java
+++ architecture-spring-elasticsearch/src/main/java/com/fast/elasticsearch/ArchitectureSpringElasticsearchApplication.java
@@ -3,6 +3,11 @@ package com.fast.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * elasticsearch 版本7.10.1
+ * lucene_version 版本8.7.0
+ * Spring Boot Starter AMQP 版本2.4.1
+ */
 @SpringBootApplication
 public class ArchitectureSpringElasticsearchApplication {
 
