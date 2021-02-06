6f902fa0f4906ee9343049f8f22d1dfba306c9a0
zyx <zhangyixiong5827@dingtalk.com>
2021-02-06 16:52:15 +0800

    提交测试

--- architecture-spring-aop/src/main/java/com/fast/architecturespringaop/dynamic/cglib/Main.java
+++ architecture-spring-aop/src/main/java/com/fast/architecturespringaop/dynamic/cglib/Main.java
@@ -4,6 +4,7 @@ package com.fast.architecturespringaop.dynamic.cglib;
 import net.sf.cglib.proxy.Enhancer;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Enhancer enhancer = new Enhancer();
