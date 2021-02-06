f4e5ce01c2a6a1d5fbeabc196a3bd2c73d5204cd
boyi.chen <495057454@qq.com>
2021-02-06 16:51:20 +0800

    创建mongodb使用案例

---
+++ architecture-spring-mongodb/src/main/java/com/fast/mongodb/ArchitectureSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.fast.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureSpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureSpringMongodbApplication.class, args);
+    }
+
+}
