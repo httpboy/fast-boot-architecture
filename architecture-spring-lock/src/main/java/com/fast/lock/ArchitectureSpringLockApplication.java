248e53d80e4be4df2bc5133c943fbdcf1886d664
boyi.chen <boyi.chen@luckincoffee.com>
2021-05-14 20:26:15 +0800

    修改文件名

--- architecture-spring-lock/src/main/java/com/fast/lock/ArchitectureSpringLockApplication.java
+++ architecture-spring-lock/src/main/java/com/fast/lock/ArchitectureSpringLockApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArchituctureSpringLockApplication {
+public class ArchitectureSpringLockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArchituctureSpringLockApplication.class, args);
+        SpringApplication.run(ArchitectureSpringLockApplication.class, args);
     }
 
 }
